681603e16738a3bf143a4e9babc59123f692ab9e
Qianchen <hieverest@foxmail.com>
2018-04-01 21:46:29 -0700

    Set default values for settings redux store

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -1,14 +1,23 @@
 import T from '../constants';
 
-const focusTime = 1, shortBreak = 1, longBreak = 2, totalSessions = 2;
+const defaultValues = {
+  focusTime: 0.2,
+  shortBreak: 0.1,
+  longBreak: 1,
+  totalSessions: 2,
+  modalOpen: false,
+}
 
-export default (state = { focusTime, shortBreak, longBreak, totalSessions }, action) => {
+export default (state = { ...defaultValues }, action) => {
   switch(action.type) {
     case T.SUBMIT_SETTINGS:
+    case T.OPEN_SETTINGS:
+    case T.CLOSE_SETTINGS:
       return ({
         ...state,
         ...action.payload
       })
+    
     default: 
       return state;
   }
