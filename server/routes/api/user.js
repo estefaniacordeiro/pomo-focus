ff1b5eae6ac808cb10dd85efef852304d6666052
Qianchen <hieverest@foxmail.com>
2018-04-03 16:45:47 -0700

    Add get current user api

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -4,8 +4,24 @@ const passport = require('passport');
 const User = mongoose.model('User');
 const auth = require('../auth');
 
+// Get user email and token
+router.get('/', auth.required, (req, res, next) => {
+  User.findById(req.payload.id).then( user => {
+    if (!user) {
+      return res.sendStatus(422);
+    }
 
+    return res.json({ user: user.toAuthJSON()});
+  }).catch(next);
+});
+
+// Register
 router.post('/', (req, res, next) => {
+  User.findOne({email: req.body.user.email }, (err, doc) => {
+    if(doc) {
+      return next({status: 422, message: "Email existed!"});
+    }
+  })
   const user = new User();
 
   user.email = req.body.user.email;
@@ -17,6 +33,7 @@ router.post('/', (req, res, next) => {
 
 })
 
+// Login
 router.post('/login', (req, res, next) => {
   if (!req.body.user.email) {
     return res.status(422).json({ errors: {email: "can't be blank"}});
