004e16f35f1555b2aa9184dd3ab41643c871e513
Qianchen <hieverest@foxmail.com>
2018-04-04 21:38:25 -0700

    Add some actions

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -2,12 +2,34 @@ export const SET_TICKING = 'SET_TICKING';
 
 
 export const SET_TIMER = 'SET_TIMER';
+export const TIMER_LOAD = 'TIMER_LOAD';
+export const TIMER_UNLOAD = 'TIMER_UNLOAD';
 export const SET_MODE = 'SET_MODE';
 export const SET_SESSION_NUMBER = 'SET_SESSION_NUMBER';
 
 export const SUBMIT_SETTINGS = 'SUBMIT_SETTINGS';
+export const OPEN_SETTINGS = 'OPEN_SETTINGS';
+export const CLOSE_SETTINGS = 'CLOSE_SETTINGS';
 
 export const ADD_TASK = 'ADD_TASK';
 export const SET_CURRENT_TASK = 'SET_CURRENT_TASK'; 
+export const GET_ALL_TASKS = 'GET_ALL_TASKS';
 
+export const GET_ALL_STATS = 'GET_ALL_STATS';
 export const ADD_STATS = 'ADD_STATS';
+
+export const REGISTER = 'REGISTER';
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+
+export const ASYNC_START = 'ASYNC_START';
+export const ASYNC_END = 'ASYNC_END';
+
+export const APP_LOAD = 'APP_LOAD';
+export const REDIRECT = 'REDIRECT';
+
+export const CLEAR_ERROR = 'CLEAR_ERROR';
+
+export const GET_SETTINGS = 'GET_SETTINGS';
+export const GET_TASKS = 'GET_TASKS';
+
