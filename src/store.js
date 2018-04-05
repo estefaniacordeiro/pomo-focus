55676b42fdd9cc3b0600d184277b6430b5ff8478
Qianchen <hieverest@foxmail.com>
2018-04-04 21:39:48 -0700

    Set up Redux middlewares

--- src/store.js
+++ src/store.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducers';
+import { promiseMiddleware, localStorageMiddleware } from './middleware';
 
 
 // const myRouterMiddleware = routerMiddleware(history);
 
 const composeEnhancers = window.window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(
-  applyMiddleware()
+  applyMiddleware(promiseMiddleware, localStorageMiddleware)
 ));
 
 
