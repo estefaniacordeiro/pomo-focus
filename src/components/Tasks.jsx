868c63f79e94d4d2818a76fa5ec17b7060e9c4ba
Qianchen <hieverest@foxmail.com>
2018-04-05 22:39:06 -0700

    Add feature: only logged in user can use task

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Input, List } from 'antd';
+import { Input, List, message } from 'antd';
 import { connect } from 'react-redux';
 import ACTION from '../constants';
 import '../css/Tasks.css';
 import agent from '../agent';
 
 const mapStateToProps = state => ({
-  tasks: [...state.tasks]
+  tasks: [...state.tasks],
+  user: state.common.user
 })
 
 const mapDispatchToProps = dispatch => ({
-  getAllTasks: () => dispatch({ type: ACTION.GET_ALL_TASKS, payload: agent.Tasks.all() }),
   addTask: newTask => dispatch({type: ACTION.ADD_TASK, payload: agent.Tasks.addNewTask(newTask) }),
   setCurrentTask: (id, lastUpdated, tasks) => 
     dispatch({type: ACTION.SET_CURRENT_TASK, payload: agent.Tasks.setCurrentTasks(id, lastUpdated), tasks})
@@ -26,10 +26,6 @@ class Tasks extends React.Component {
       inputValue: tasks.length > 0 ? tasks.slice(-1)[0].name : null,
       showList: false
     }
-  }
-
-  componentWillMount() {
-    this.props.getAllTasks();
   }
 
   componentWillReceiveProps(nextProps) {
@@ -60,6 +56,10 @@ class Tasks extends React.Component {
   }
 
   onSubmit = e => {
+    if (!this.props.user) {
+      this.warnUserToSignIn();
+      return;
+    }
     const { addTask } = this.props;
     const value = e.target.value;
     const newTask = {
@@ -98,12 +98,16 @@ class Tasks extends React.Component {
     this.closeTasksList();
   }
 
+  warnUserToSignIn =() => {
+    message.warning('You need sign in to use the task feature :)');
+  }
+
   render() {
     const { inputValue, showList } = this.state;
     const tasks = this.props.tasks.slice(0, -1).reverse();
 
     return (
-      <div className='Tasks-wrapper'>
+      <div className='Tasks-wrapper' onMouseLeave={this.closeTasksList} >
         <Input 
           size='large'
           suffix={<i className="fas fa-tasks Tasks-button" onMouseOver={this.openTasksList}/>}
@@ -116,7 +120,7 @@ class Tasks extends React.Component {
         />
         {showList ? 
         <List 
-          onMouseLeave={this.closeTasksList}
+          className="Tasks-list"
           bordered
           dataSource={ tasks }
           renderItem={ item => {
