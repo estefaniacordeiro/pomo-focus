2ec692d2fbc1380bab51e694be2172d5d30a086e
Qianchen <hieverest@foxmail.com>
2018-04-16 01:14:29 -0700

    Fix bug: fetch tasks and stats in Home after redirecting to it

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import Timer from './Timer';
 import TaskSelect from './TaskSelect';
+import ACTION from '../constants';
+import agent from '../agent';
 import '../css/Home.css';
 
+const mapStateToProps = state => ({
+  user: state.common.user
+});
+
+const mapDispatchToProps = dispatch => ({
+  getAllTasks: () => dispatch({ type: ACTION.GET_ALL_TASKS, payload: agent.Tasks.all() }),
+  getSettings: () => dispatch({type: ACTION.GET_SETTINGS, payload: agent.Settings.current() }),
+});
+
 class Home extends React.Component {
+
+  componentWillMount() {
+    const { user, getAllTasks, getSettings } = this.props;
+    if (user) {
+      getAllTasks();
+      getSettings();
+    }
+  }
 
   render() {
     return (
@@ -16,4 +35,4 @@ class Home extends React.Component {
   }
 }
 
-export default Home;
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
